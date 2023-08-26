225d2e7b66a553e0298e6ca841da46dabe9e6526
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-26 19:01:25 -0300

    up

---
+++ src/aulas/1.0.introducao/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+const msg = 'teste';
+
+const pessoa = ["André Rafael", 29];
+const pessoa2 = {
+  data: {
+    nome: "André Rafael",
+    idade: 29,
+  },
+
+}
+pessoa2.data.idade = 30;
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <div>
+    {{ pessoa[0] }}
+  </div>
+  <div>
+    {{ pessoa2.data.idade }}
+  </div>
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './aulas/1.0.introducao/App.vue'
 
 createApp(App).mount('#app')
